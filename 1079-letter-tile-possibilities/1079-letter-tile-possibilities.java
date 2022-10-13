0d24817d304d0efe5b63e4f7f8d0a63d566aea01
Nitish baghel <80021632+Nitishbaghel@users.noreply.github.com>
2022-10-13 15:42:50 +0530

    Time: 285 ms (5.16%), Space: 49.6 MB (42.58%) - LeetHub

---
+++ 1079-letter-tile-possibilities/1079-letter-tile-possibilities.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int numTilePossibilities(String str) {
+        int freq[]=new int[26];
+        for (int i = 0; i < str.length(); i++) {
+            freq[str.charAt(i)-'A']++;
+        }
+        return tilepossibility(freq,"");
+    }
+    public static int tilepossibility(int freq[], String ans) {
+        int c=0;
+        System.out.println(ans);
+        for (int i = 0; i < freq.length; i++) {
+            if(freq[i]>=1)
+            {
+                char ch=(char) (i+'A');
+                freq[i]--;
+                c+= tilepossibility(freq,ans+ch)+1;
+                freq[i]++;
+            }
+        }
+        return c;
+    }
+}
