f4bbbdadb7c4f681063cb0a56d8c7af15a5bc9c3
Nitish baghel <80021632+Nitishbaghel@users.noreply.github.com>
2023-07-05 15:53:25 +0530

    Time: 4 ms (36.23%), Space: 5.9 MB (58.57%) - LeetHub

---
+++ 2429-minimize-xor/2429-minimize-xor.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    
+	// O(logn)
+    int countSetBits(int n){
+        int c=0;
+        while(n>0){
+            int rmsb=n&(-n); // rightmost set bit
+            n-=rmsb;
+            c++;
+        }
+        return c;
+    }
+    
+    int minimizeXor(int num1, int num2) {
+        int c=countSetBits(num2);
+        
+        int x=0; // building answer by nullifying MSB's of 'num1' for 'Overall Minimum XOR'
+        for(int bit=31;bit>=0;bit--){
+            if((num1 & (1<<bit))>0 && c>0){
+                x |= (1<<bit);
+                c--;
+            }
+        }
+        
+	   // setting LSB's of 'x' so that 'count of set bits are same as num2' & keeping value of 'x' as minimum possible 
+        for(int bit=0;bit<=31;bit++){
+            if(c>0 && (x & (1<<bit))==0){
+                x |= (1<<bit);
+                c--;
+            }
+        }
+        return x;
+    }
+};
